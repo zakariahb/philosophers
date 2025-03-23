db28e5654d4e89c255a31bc1dabb015213f37d7b
Zakaria Laksyar <zalaksya@e1r7p2.1337.ma>
2025-03-23 03:37:26 +0000

    philo

---
+++ list.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zalaksya <zalaksya@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/22 16:09:19 by zalaksya          #+#    #+#             */
+/*   Updated: 2025/03/23 03:05:18 by zalaksya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+t_philo	*ft_lstlast(t_philo *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next != NULL)
+	{
+		lst = lst->next;
+	}
+	return (lst);
+}
+
+t_philo	*ft_lstnew(char **ar)
+{
+	static int	i;
+	t_philo	*head;
+
+	i = 0;
+	head = malloc(sizeof(t_philo));
+	if (!head)
+		return (NULL);
+	head->id = i + 1;
+	head->t_die = ft_atoi(ar[1]) * 1000;
+	head->t_eat = ft_atoi(ar[2]) * 1000;
+	head->t_sleep = ft_atoi(ar[3]) * 1000;
+	if (ar[4])
+		head->t_t_eat = ft_atoi(ar[4]);
+	head -> next = NULL;
+	return (head);
+}
+
+void	ft_lstadd_back(t_philo **lst, t_philo *new)
+{
+	t_philo	*temp;
+
+	if (!lst || !new)
+		return ;
+	temp = ft_lstlast(*lst);
+	if (!temp)
+		*lst = new;
+	else
+		temp->next = new;
+}
