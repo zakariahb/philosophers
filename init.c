c11a71d6d4afe4439ea9922d05ff5561ed807456
Zakaria Laksyar <zalaksya@e1r8p7.1337.ma>
2025-07-10 14:35:21 +0100

    philo

--- init.c
+++ init.c
@@ -73,7 +73,6 @@ int	init_philos(t_data **data)
 	while (i < (*data)->n_philo)
 	{
 		(*data)->philos[i].id = i + 1;
-		(*data)->philos[i].eating = 0;
 		(*data)->philos[i].meals_eaten = 0;
 		(*data)->philos[i].last_meal_time = 0;
 		(*data)->philos[i].l_fork = &(*data)->forks[i];
